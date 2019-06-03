ce5a02b7f3115ccb34bfd0608e47f18dab5e3e4e
KevinCanacuanPasquel <kfc0_10@hotmail.com>
2019-06-03 06:16:21 -0500

    Version 2.. aun funciona xd

---
+++ Deberes/Examen IB/app/src/main/java/com/example/examenib/ConsultarJugadorActivity.kt
@@ -0,0 +1,47 @@
+package com.example.examenib
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_consultar_jugador.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class ConsultarJugadorActivity : AppCompatActivity() {
+    var padreId : Int = 0
+    var usuario :String = "";
+    var equipoRespaldo : EquipoFutbol? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        usuario = intent.getStringExtra("usuario").toString()
+        equipoRespaldo = intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        padreId = intent.getIntExtra("padreId", -1)
+        val equipo=intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        setContentView(R.layout.activity_consultar_jugador)
+        val nombre=equipo.nombre.toString();
+        val adapter = ArrayAdapter<Jugador>(
+            this,
+            android.R.layout.simple_list_item_1,
+            BDJugador.mostrarJugador(padreId)
+        )
+        lstJugador.adapter = adapter;
+        lstJugador.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+
+            val jugadorSeleccionado = parent.getItemAtPosition(position) as Jugador
+            val intentJugadorSeleccionado = Intent(this, ActualizarJugadorActivity::class.java)
+            intentJugadorSeleccionado.putExtra("usuario", usuario)
+            intentJugadorSeleccionado.putExtra("Jugador", jugadorSeleccionado)
+            intentJugadorSeleccionado.putExtra("EquipoRespaldo", equipoRespaldo)
+            startActivity(intentJugadorSeleccionado)
+        }
+        textView12.setText(getString(R.string.nombreJugador)+": "+nombre)
+    }
+    override fun onBackPressed() {
+        val intentMenu = Intent(this, ActualizarActivity::class.java)
+        intentMenu.putExtra("Equipo", equipoRespaldo)
+        intentMenu.putExtra("usuario", usuario)
+        startActivity(intentMenu)
+    }
+}
---
+++ Deberes/Examen IB/app/src/main/java/com/example/examenib/IngresarJugadorActivity.kt
@@ -0,0 +1,51 @@
+package com.example.examenib
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_ingresar_jugador.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class IngresarJugadorActivity : AppCompatActivity() {
+    var padreId : Int = 0
+    var usuario :String = "";
+    var equipoRespaldo : EquipoFutbol? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_jugador)
+        usuario = intent.getStringExtra("usuario").toString()
+        equipoRespaldo = intent.getParcelableExtra<EquipoFutbol>("EquipoRespaldo")
+        padreId = intent.getIntExtra("padreId", -1)
+        btnGuardar.setOnClickListener { guardarJugador() }
+    }
+
+    fun guardarJugador(){
+        val jugador = Jugador(
+            id = null,
+            numeroCamiseta = txtnumeroCamiseta.text.toString().toInt(),
+            nombreCamiseta = txtNombreCamiseta.text.toString(),
+            nombreCompletoJugador = txtNombreJugador.text.toString(),
+            poderEspecialDos = txtpoderEspecialDos.text.toString(),
+            fechaIngresoEquipo = txtfechaIngresoEquipo.text.toString(),
+            goles = txtGoles.text.toString().toInt(),
+            equipoFutbolId = padreId
+        )
+        BDJugador.agregarJugador(jugador)
+        Toast.makeText(this, getString(R.string.msg)+" "+usuario, Toast.LENGTH_SHORT).show()
+        val retorno = Intent(this, ConsultarJugadorActivity::class.java)
+        retorno.putExtra("usuario", usuario)
+        retorno.putExtra("padreId", padreId)
+        retorno.putExtra("EquipoRespaldo", equipoRespaldo)
+        startActivity(retorno)
+    }
+    override fun onBackPressed() {
+
+        val intentMenu = Intent(this, ActualizarActivity::class.java)
+        intentMenu.putExtra("usuario", usuario)
+
+        intentMenu.putExtra("Equipo", equipoRespaldo)
+
+        startActivity(intentMenu)
+    }
+}
